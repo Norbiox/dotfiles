6739eea13bfea409f1f549edb72ada2e1c37b977
Norbiox <norbertchmiel.it@gmail.com>
2022-09-27 00:54:54 +0200

    add github configuration to install script

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -5,4 +5,7 @@ git config --global user.name "Norbiox"
 git config --global user.email "norbertchmiel.it@gmail.com"
 git config --global core.editor "vim"
 
+sudo pacman -S github-cli
+gh auth login
+
 echo "Done"
