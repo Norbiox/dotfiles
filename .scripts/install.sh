9e2c53108cf7bd7cdc38960d3abdc244b7e424c1
Norbert Chmiel <norbertchmiel.it@gmail.com>
2022-09-27 11:37:35 +0200

    add VictorMono font installation

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -28,6 +28,14 @@ echo "Install various packages from AUR..."
 yay -S \
   hstr
 
+echo "Install additional fonts..."
+if [ ! -d "/usr/local/share/fonts" ]; then
+  sudo mkdir /usr/local/share/fonts
+fi
+curl -s https://rubjo.github.io/victor-mono/VictorMonoAll.zip --output VictorMonoAll.zip
+sudo bsdtar -xf- VictorMonoAll.zip -C /usr/local/share/fonts
+fc-cache
+
 echo "Setup git..."
 git config --global user.name "Norbiox"
 git config --global user.email "norbertchmiel.it@gmail.com"
