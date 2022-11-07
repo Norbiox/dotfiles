75cb350682f0b43149dd3a121d5b433f6600f856
Norbert Chmiel <norbertchmiel.it@gmail.com>
2022-11-07 15:32:05 +0100

    move nitrogen restore to layout scripts

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -10,4 +10,5 @@ xrandr --output DP2-2 --mode 1920x1200 --pos 3840x0 --rotate normal
 xrandr --output DP2-3 --primary --mode 1920x1200 --pos 1920x0 --rotate normal
 xrandr --output eDP1 --mode 1920x1080 --pos 1920x1200 --rotate normal
 
+sleep 3
 nitrogen --restore
