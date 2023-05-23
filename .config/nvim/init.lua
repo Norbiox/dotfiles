0f31c8ea1a776ed3a4e3f72e4807662bd940b8a0
Norbert Chmiel <norbertchmiel.it@gmail.com>
2023-05-23 06:44:10 +0200

    nvim - fix lspconfig for lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -503,7 +503,7 @@ require('mason').setup({
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua', 'gopls', 'clojure_lsp'}
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'lua_ls', 'gopls', 'clojure_lsp'}
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
@@ -531,7 +531,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
