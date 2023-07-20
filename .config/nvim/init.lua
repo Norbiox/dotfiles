07858e9668fa135bebffc7ad4c485a0e86e31bcf
Norbert Chmiel <norbertchmiel.it@gmail.com>
2023-07-20 11:55:14 +0200

    add lualine sections configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -303,6 +303,13 @@ require('lualine').setup {
     theme = 'onedark',
     component_separators = '|',
     section_separators = '',
+    sections = {
+      lualine_a = {'mode'},
+      lualine_c = {'branch', 'diff', 'diagnostics'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+    },
   }
 }
 
