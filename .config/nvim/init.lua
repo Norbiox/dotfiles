bae6882e170ff8cd35080009780ae4288592e19e
Norbert Chmiel <norbertchmiel.it@gmail.com>
2024-09-03 14:16:12 +0200

    udpate config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -499,7 +499,7 @@ vim.keymap.set('n', '<leader>/', function()
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles', hidden=True })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
@@ -535,7 +535,6 @@ require('nvim-treesitter.configs').setup {
     'gpg',
     'groovy',
     'gleam',
-    'help',
     'html',
     'ini',
     'json',
