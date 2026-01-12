5813129bbc4f86dadde4160be028739a7d035244
Norbert Chmiel <norbertchmiel.it@gmail.com>
2026-01-12 10:48:12 +0100

    add scripts for managing workspaces

--- .screenlayout/one_above.sh
+++ .screenlayout/one_above.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-xrandr \
-  --output DP1 --off \
-  --output DP2 --off \
-  --output HDMI1 --on \
-  --output VIRTUAL1 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x1200 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-2-1 --off --output DP-2-2 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2-3 --off
 
-xrandr --output HDMI1 --mode 1920x1200 --pos 0x0    --rotate normal --primary
-xrandr --output eDP1  --mode 1920x1080 --pos 0x1200 --rotate normal
-
-# sleep 3
-# nitrogen --restore
+sleep 3
+nitrogen --restore
--- .screenlayout/single.sh
+++ .screenlayout/single.sh
@@ -3,9 +3,9 @@ xrandr \
   --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
   --output DP1 --off \
   --output DP2 --off \
-  --output DP2-1 --off \
-  --output DP2-2 --off \
-  --output DP2-3 --off \
+  --output DP-2-1 --off \
+  --output DP-2-2 --off \
+  --output DP-2-3 --off \
   --output HDMI1 --off \
   --output VIRTUAL1 --off
 
